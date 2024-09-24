6027fe58bc7b9c5e4ecbcbe18721fb6d6b3137c6
ozaaso <ozaaso@gmail.com>
2024-09-24 19:07:07 +0700

    mvc episode 3

---
+++ app/core/App.php
@@ -0,0 +1,19 @@
+<?php
+
+class App {
+    public function __construct() 
+    {
+        $url = $this->parseURL();
+       var_dump($url);
+    }
+
+    public function parseURL()
+    {
+        if(isset($_GET['url'])){
+            $url = rtrim($_GET['url'], '/');
+            $url = filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,7 +1,5 @@
 <?php
 
-require_once __DIR__ . '/../app/Controllers/FormController.php';
-require_once __DIR__ . '/../routes/web.php'; // Muat file routing
+require_once '../app/init.php';
 
-// Tidak perlu ada logika routing tambahan di sini karena sudah ditangani oleh routes/web.php
-?>
+$app = new App;
